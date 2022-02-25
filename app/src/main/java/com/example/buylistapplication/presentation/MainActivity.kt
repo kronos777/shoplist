5dbfa6e310084b40341d188d65722a42392ff21a
Kronos777 <ziswp1@gmail.com>
2022-02-25 20:31:52 +0300

    add data class

--- app/src/main/java/com/example/buylistapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/buylistapplication/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.buylistapplication
+package com.example.buylistapplication.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.buylistapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

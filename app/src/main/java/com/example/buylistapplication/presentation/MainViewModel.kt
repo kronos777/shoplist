053395a46deeb73181ac7a6ba574c12726016a3e
Kronos777 <ziswp1@gmail.com>
2022-02-26 17:23:20 +0300

    ready first presentation layer

---
+++ app/src/main/java/com/example/buylistapplication/presentation/MainViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.buylistapplication.presentation
+
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.buylistapplication.data.ShopListRepositoryImpl
+import com.example.buylistapplication.domain.*
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+
+    //создаем объект livedata
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList() {
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+
+
+
+}

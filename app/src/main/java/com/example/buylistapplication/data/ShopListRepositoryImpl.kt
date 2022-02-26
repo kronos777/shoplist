abea58727deded0756ea84a13c90ab03778eb5db
Kronos777 <ziswp1@gmail.com>
2022-02-26 19:14:45 +0300

    new layout and shoplist fun

--- app/src/main/java/com/example/buylistapplication/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/buylistapplication/data/ShopListRepositoryImpl.kt
@@ -8,7 +8,7 @@ import java.lang.RuntimeException
 
 object ShopListRepositoryImpl: ShopListRepository {
 
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({ p0, p1 -> p0.id.compareTo(p1.id) })
     private var autoIncrementId = 0
     private val shopListLD = MutableLiveData<List<ShopItem>>()
     

295ccd53022a439c1352d740486af85e45a7d4d8
Kronos777 <ziswp1@gmail.com>
2022-02-28 17:36:46 +0300

    ready ViewModelAddShopItem

---
+++ app/src/main/java/com/example/buylistapplication/presentation/ShopItemActivity.kt
@@ -0,0 +1,17 @@
+package com.example.buylistapplication.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.buylistapplication.R
+
+class ShopItemActivity : AppCompatActivity() {
+
+
+    private lateinit var viewModel: ShopItemViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+        //viewModel.errorInputName.value = false
+    }
+}
